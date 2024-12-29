fd863665dc3c9478f7daeeb2d1e80bd173f654d7
jake0319 <shjun1411@naver.com>
2024-12-29 22:40:21 +0900

    add shadcn card

--- src/features/kanban-board/ui/kanban-board.tsx
+++ src/features/kanban-board/ui/kanban-board.tsx
@@ -1,33 +1,13 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
 import { DndContext, closestCenter, type DragEndEvent } from "@dnd-kit/core";
 import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
-
-interface Task {
-	id: string;
-	columnId: string;
-	content: string;
-}
-
-function SortableItem({ task }: { task: Task }) {
-	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: task.id });
-
-	const style = {
-		transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-		transition,
-	};
-
-	return (
-		<div ref={setNodeRef} style={style} {...attributes} {...listeners} className="p-4 mb-2 bg-white rounded shadow border border-solid border-gray-600">
-			{task.content}
-		</div>
-	);
-}
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@components/ui/card";
 
 export default function KanbanBoard() {
 	const [tasks, setTasks] = useState<Task[]>(initialTasks);
 	const [mount, setMount] = useState(false);
+
 	useEffect(() => setMount(true), []);
 
 	function handleDragEnd(event: DragEndEvent) {
@@ -60,20 +40,56 @@ export default function KanbanBoard() {
 	);
 }
 
+interface Task {
+	id: string;
+	content: string;
+}
+
+function SortableItem({ task }: { task: Task }) {
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: task.id });
+
+	const style = {
+		transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
+		transition,
+	};
+
+	return (
+		<div ref={setNodeRef} style={style} {...attributes} {...listeners} className="p-4 mb-2 bg-white rounded shadow border border-solid border-gray-600">
+			<CardComponent task={task}></CardComponent>
+		</div>
+	);
+}
+
 const initialTasks: Task[] = [
 	{
 		id: "task1",
-		columnId: "done1",
 		content: "sortalbe item1",
 	},
 	{
 		id: "task2",
-		columnId: "done2",
 		content: "sortalbe item2",
 	},
 	{
 		id: "task3",
-		columnId: "done3",
 		content: "sortalbe item3",
 	},
 ];
+
+export function CardComponent({ task }: { task: Task }) {
+	return (
+		<Card className="w-[300px]">
+			<CardHeader>
+				<CardTitle>{task.content}</CardTitle>
+				<CardDescription>Deploy your new project in one-click.</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form>
+					<div className="grid w-full items-center gap-4">
+						<div className="flex flex-col space-y-1.5"></div>
+					</div>
+				</form>
+			</CardContent>
+			<CardFooter className="flex justify-between"></CardFooter>
+		</Card>
+	);
+}
